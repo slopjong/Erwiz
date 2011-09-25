1e800e3dadc4d78173c7b2b635d1047f479e57ba
Slopjong <slopjong@hackerspace.lu>
2011-09-25 06:14:34 +0200

    Improved the build/release files and deleted unnecessary stuff.

--- erwiz-tool/Erwiz/releases/linux/archlinux/replace_md5.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-echo "sed -i \"s/^md5sums=(\(.*\))/\md5sums=($1)/g\" $2"
-sed -i "s/^md5sums=(\(.*\))/\md5sums=($1)/g" $2
--- erwiz-tool/Erwiz/releases/linux/archlinux/replace_version_number.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-echo "sed -i \"s/^pkgver=\(.*\)/\pkgver=($1)/g\" $2"
-sed -i "s/^pkgver=\(.*\)/\pkgver=($1)/g" $2
